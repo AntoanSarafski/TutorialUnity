3baa7b3fa1eec40476742e935cd39b2dac1e828d
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2022-10-18 14:56:26 +0300

    Added fixed End of the game and Restart Game.

--- Unity Example/Assets/Scripts/PlayerCollision.cs
+++ Unity Example/Assets/Scripts/PlayerCollision.cs
@@ -5,7 +5,7 @@ public class PlayerCollision : MonoBehaviour
 {
 
     public PlayerMovement movement;
-
+    public GameManager gameManager;
 
     public void OnCollisionEnter(Collision collision)
     {
