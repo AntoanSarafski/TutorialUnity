889979c4939d0c04aea61956bb6889fc636a24fb
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2022-10-18 17:59:26 +0300

    Added NextScene loading funcion in the animation.
    If check must be include for this NextSceneLoading function.

--- Unity Example/Assets/Scripts/PlayerCollision.cs
+++ Unity Example/Assets/Scripts/PlayerCollision.cs
@@ -5,7 +5,6 @@ public class PlayerCollision : MonoBehaviour
 {
 
     public PlayerMovement movement;
-    public GameManager gameManager;
 
     public void OnCollisionEnter(Collision collision)
     {
